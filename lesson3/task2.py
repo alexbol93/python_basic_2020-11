6167963979e61fe76b2d285f101c6998f2cb676a
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2020-12-23 00:44:08 +0300

    add depth level visualization

--- lesson3/task2.py
+++ lesson3/task2.py
@@ -2,14 +2,18 @@ from functools import wraps
 
 
 def trace(func):
+    trace.recursion_depth = 0
     @wraps(func)
     def wrapper(*args, **kwargs):
+        symb = '__'
         args_repr = [repr(a) for a in args]
         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
         signature = ", ".join(args_repr + kwargs_repr)
-        print(f'-->{func.__name__}({signature})')
+        print(f'{symb * trace.recursion_depth}-->{func.__name__}({signature})')
+        trace.recursion_depth += 1
         value = func(*args, **kwargs)
-        print(f'<--{func.__name__}({signature}) == {value}')
+        trace.recursion_depth -= 1
+        print(f'{symb * trace.recursion_depth}<--{func.__name__}({signature}) == {value}')
         return value
     return wrapper
 
